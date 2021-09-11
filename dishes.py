b4e7f1c147b7b4ccb076eb75ef8676d6b6b30391
bhvay <sreebhavya99033@gmail.com>
2021-09-11 11:26:34 +0530

    my first commit for dishes

--- dishes.py
+++ dishes.py
@@ -6,6 +6,8 @@ karnataka_dishes=["ragimuddha","ragragiidosa"]
 
 kerala_dishes=["putt","avial","aaapam"]
 
+bengali_dishes=["bengali sweets","rasagulla"]
+
 north_indian_dishes=["paratha","naan","roti","chapathi"]
 dish=input("enter any dish")
 if dish in tamil_dishes:
@@ -20,5 +22,7 @@ elif dish in kerala_dishes:
     print("it is a kerala dish")
 elif dish in north_indian_dishes:
     print("it is a north indian dish")
+elif dish in bengali_dishes:
+    print("it is a bengali dish")
 else:
     print("no dish in any culture")
