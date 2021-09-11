289141f46116c2697dce725b0454c8c7e4b9cc9f
bhvay <sreebhavya99033@gmail.com>
2021-09-11 11:13:24 +0530

    Sample change

---
+++ dishes.py
@@ -0,0 +1,24 @@
+tamil_dishes=["idly","sambar","idiappam"]
+
+Andhra_dishes=["gongoor pickle","dosa","natu kodi chicken"]
+
+karnataka_dishes=["ragimuddha","ragragiidosa"]
+
+kerala_dishes=["putt","avial","aaapam"]
+
+north_indian_dishes=["paratha","naan","roti","chapathi"]
+dish=input("enter any dish")
+if dish in tamil_dishes:
+
+      print(" It is a tamil dish")
+elif dish in Andhra_dishes:
+    print("it is a andhra dish")
+
+elif dish in karnataka_dishes:
+    print(" it is karnataka dish")
+elif dish in kerala_dishes:
+    print("it is a kerala dish")
+elif dish in north_indian_dishes:
+    print("it is a north indian dish")
+else:
+    print("no dish in any culture")
